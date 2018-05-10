40ef16badb762c98b5ca680f0d99d584d3fe5265
aliugenb <yeliu89@sina.com>
2018-05-10 23:53:57 +0800

    first commit

---
+++ src/BinarySearch.java
@@ -0,0 +1,32 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Author: ye.liu
+ * Date: 2018/5/10
+ * Time: 23:21
+ */
+
+public class BinarySearch {
+    public static void main(String[] args) {
+        int[] a = {1,3,46,78,90,115,666,777,999};
+        int b = recursionBinarySearch(a,114,0,8);
+        System.out.println(b);
+    }
+    public static int recursionBinarySearch(int[] arr,int key,int low,int high){
+
+        if(key < arr[low] || key > arr[high] || low > high){
+            return -1;
+        }
+
+        int middle = (low + high) / 2;			//初始中间位置
+        if(arr[middle] > key){
+            //比关键字大则关键字在左区域
+            return recursionBinarySearch(arr, key, low, middle - 1);
+        }else if(arr[middle] < key){
+            //比关键字小则关键字在右区域
+            return recursionBinarySearch(arr, key, middle + 1, high);
+        }else {
+            return middle;
+        }
+
+    }
+}
---
+++ src/BubbleSort.java
@@ -0,0 +1,29 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Author: ye.liu
+ * Date: 2018/5/10
+ * Time: 22:46
+ * 冒泡排序
+ */
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] arr = {23, 4, 5, 6, 11, 3, 43, 45, 87};
+        for (int num : arr) {
+            System.out.println(num + "");
+        }
+
+        for (int i = 0; i < arr.length - 1; i++) {//外层循环控制排序趟数
+            for (int j = 0; j < arr.length - 1 - i; j++) {//内层循环控制每一趟排序多少次
+                if (arr[j] < arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        for (int num : arr) {
+            System.out.print(num + ",");
+        }
+    }
+}
