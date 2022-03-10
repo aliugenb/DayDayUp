cb0115188ad3429bc57856904b7c823dc7fa2146
liuye <yeliu89@sina.com>
2022-03-10 23:17:24 +0800

    add LongestCommonPrefix offical

--- src/com/study/leetcode/LongestCommonPrefix.java
+++ src/com/study/leetcode/LongestCommonPrefix.java
@@ -41,9 +41,27 @@ public class LongestCommonPrefix {
         return res;
     }
 
+    public static String offical(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
+        int length = strs[0].length();
+        int count = strs.length;
+        for (int i = 0; i < length; i++) {
+            char c = strs[0].charAt(i);
+            for (int j = 1; j < count; j++) {
+                if (i == strs[j].length() || strs[j].charAt(i) != c) {
+                    return strs[0].substring(0, i);
+                }
+            }
+        }
+        return strs[0];
+    }
+
     public static void main(String[] args) {
-//        String[] s = {"abcdd", "abcccd", "abddddd"};
-        String[] s = {"cir","car"};
+        String[] s = {"abcdd", "abcccd", "abddddd"};
+//        String[] s = {"cir", "car"};
         System.out.println(commonPrefix(s));
+        System.out.println(offical(s));
     }
 }
