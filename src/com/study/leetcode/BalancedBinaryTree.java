0ecb22c02f9de4b7dff4dea88e1b9f06162e36ba
aliugenb <yeliu89@sina.com>
2022-03-14 22:28:17 +0800

    add BanlancedBinaryTree

---
+++ src/com/study/leetcode/BalancedBinaryTree.java
@@ -0,0 +1,43 @@
+package com.study.leetcode;
+
+/**
+ * ClassName: BalancedBinaryTree
+ * Author: ye.liu
+ * Date: 2022/3/14 20:29
+ * Description:
+ */
+
+/*
+输入一棵二叉树的根节点，判断该树是不是平衡二叉树。
+如果某二叉树中任意节点的左右子树的深度相差不超过1，那么它就是一棵平衡二叉树。
+ */
+
+public class BalancedBinaryTree {
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public static boolean isBalanced(TreeNode root) {
+        if (root == null) {
+            return true;
+        } else {
+            return Math.abs(height(root.left) - height(root.right)) <= 1
+                    && isBalanced(root.right)
+                    && isBalanced(root.left);
+        }
+    }
+
+    public static int height(TreeNode root) {
+        if (root == null) {
+            return 0;
+        } else {
+            return Math.max(height(root.left), height(root.right)) + 1;
+        }
+    }
+}
