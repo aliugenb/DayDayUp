f270e16b2a8024b5adab4222b069f164c0a389cd
aliugenb <yeliu89@sina.com>
2022-03-15 14:53:42 +0800

    fix

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -12,9 +12,10 @@ public class BubbleSort {
         for (int num : arr) {
             System.out.println(num + "");
         }
-
-        for (int i = 0; i < arr.length - 1; i++) {//外层循环控制排序趟数
-            for (int j = 0; j < arr.length - 1 - i; j++) {//内层循环控制每一趟排序多少次
+        //外层循环控制排序趟数
+        for (int i = 0; i < arr.length - 1; i++) {
+            //内层循环控制每一趟排序多少次
+            for (int j = 0; j < arr.length - 1 - i; j++) {
                 if (arr[j] < arr[j + 1]) {
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
--- src/QuickSort.java
+++ src/QuickSort.java
@@ -12,33 +12,44 @@ public class QuickSort {
     }
 
     private static void sort(int[] a, int lo, int hi) {
-        if (lo >= hi)//递归退出判断条件
+        //递归退出判断条件
+        if (lo >= hi) {
             return;
-        int p = partition(a, lo, hi);//对于某一元素，其本身不必参与递归了，因为其所在的位置已经满足前面的不大于，后面的不小于
+        }
+        //对于某一元素，其本身不必参与递归了，因为其所在的位置已经满足前面的不大于，后面的不小于
+        int p = partition(a, lo, hi);
         sort(a, lo, p - 1);
         sort(a, p + 1, hi);
     }
 
     private static int partition(int[] a, int lo, int hi) {
-        int left = lo;//左pointer，供扫描用
-        int right = hi + 1;//右pointer，供扫描用，加1是为了方便扫描的推进，
+        //左pointer，供扫描用
+        int left = lo;
+        //右pointer，供扫描用，加1是为了方便扫描的推进，
+        int right = hi + 1;
         int pivot = a[lo];
 
         while (true) {
-            while (a[++left] <= pivot)//从lo开始，找到大于pivot的元素，在访问数组时使用前++更安全，后++可能会发生越界
-                if (left == hi)//防止越界
+            //从lo开始，找到大于pivot的元素，在访问数组时使用前++更安全，后++可能会发生越界
+            while (a[++left] <= pivot) {
+                if (left == hi) {
                     break;
-            while (a[--right] >= pivot)//从hi开始，找到小于pivot的元素
-                if (right == lo)//防止越界
+                }
+            }
+            //从hi开始，找到小于pivot的元素
+            while (a[--right] >= pivot) {
+                if (right == lo) {
                     break;
-            if (left >= right)//左右扫描相交，迭代结束判断条件，相等的时候说明就是和pivot相等的元素
+                }
+            }
+            //左右扫描相交，迭代结束判断条件，相等的时候说明就是和pivot相等的元素
+            if (left >= right) {
                 break;
-            swap(a, left, right);//交换pivot前面大于pivot的元素和pivot后面小于pivot的元素，
-            //从这里可以看出快速排序不稳定，因为两者之间存在和此时的left或者right相等的元素时，原有的顺序就被破坏了
+            }
+            swap(a, left, right);
         }
-        swap(a, lo, right);//将枢轴元素放到合适的位置
-//pivot未交换到合适的位置之前，其他位置的元素都满足扫描条件了(两个while里面为真)，然后再进行一次扫描，扫描条件均为假了，right<=left，right所在位置的元素是不大于pivot的
-        return right;//返回切分元素的位置
+        swap(a, lo, right);
+        return right;
     }
 
     private static void swap(int[] a, int i, int j) {
@@ -48,7 +59,6 @@ public class QuickSort {
         a[j] = temp;
     }
 
-    //单元测试
     public static void main(String[] args) {
         int[] a = {3, 4, 1, 9, 3, 2, 1, 6, 8, 4, 7, 5};
         sort(a);
