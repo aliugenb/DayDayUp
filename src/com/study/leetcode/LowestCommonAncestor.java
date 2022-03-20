4d14f357be33e36c047c31342026ec7c24415d6c
aliugenb <yeliu89@sina.com>
2022-03-20 23:33:49 +0800

    add LowestCommonAncestor

---
+++ src/com/study/leetcode/LowestCommonAncestor.java
@@ -0,0 +1,36 @@
+package com.study.leetcode;
+
+/**
+ * ClassName: LowestCommonAncestor
+ * Author: ye.liu
+ * Date: 2022/3/20 21:09
+ * Description:
+ */
+
+/*
+给定一个二叉搜索树, 找到该树中两个指定节点的最近公共祖先。
+百度百科中最近公共祖先的定义为：
+“对于有根树 T 的两个结点 p、q，最近公共祖先表示为一个结点 x，满足 x 是 p、q 的祖先且 x 的深度尽可能大（一个节点也可以是它自己的祖先）。”
+ */
+
+public class LowestCommonAncestor {
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public static TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root.val < p.val && root.val < q.val) {
+            return lowestCommonAncestor(root.right, p, q);
+        }
+        if (root.val > p.val && root.val > q.val) {
+            return lowestCommonAncestor(root.left, p, q);
+        }
+        return root;
+    }
+}
