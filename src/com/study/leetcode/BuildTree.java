be6f67a391a0d2b29735e51b58d4f85d77209eb3
aliugenb <yeliu89@sina.com>
2022-03-21 19:27:05 +0800

    add BuildTree

---
+++ src/com/study/leetcode/BuildTree.java
@@ -0,0 +1,29 @@
+package com.study.leetcode;
+
+/**
+ * ClassName: BuildTree
+ * Author: ye.liu
+ * Date: 2022/3/20 23:34
+ * Description:
+ */
+
+/*
+输入某二叉树的前序遍历和中序遍历的结果，请构建该二叉树并返回其根节点。
+假设输入的前序遍历和中序遍历的结果中都不含重复的数字。
+ */
+
+public class BuildTree {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+
+    }
+}
