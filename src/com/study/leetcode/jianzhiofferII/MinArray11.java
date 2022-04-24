9f4864c93569fd6b576b3b6c4fd46e79a2ff27d5
aliugenb <yeliu89@sina.com>
2022-04-24 22:17:54 +0800

    add MinArray11

---
+++ src/com/study/leetcode/jianzhiofferII/MinArray11.java
@@ -0,0 +1,33 @@
+package com.study.leetcode.jianzhiofferII;
+
+/**
+ * ClassName: MinArray11
+ * Author: ye.liu
+ * Date: 2022/4/20 22:25
+ * Description:
+ */
+/*
+剑指 Offer 11. 旋转数组的最小数字
+把一个数组最开始的若干个元素搬到数组的末尾，我们称之为数组的旋转。
+给你一个可能存在重复元素值的数组numbers，它原来是一个升序排列的数组，并按上述情形进行了一次旋转。请返回旋转数组的最小元素。
+例如，数组[3,4,5,1,2] 为 [1,2,3,4,5] 的一次旋转，该数组的最小值为 1。
+注意，数组 [a[0], a[1], a[2], ..., a[n-1]] 旋转一次 的结果为数组 [a[n-1], a[0], a[1], a[2], ..., a[n-2]] 。
+ */
+
+public class MinArray11 {
+    public static int minArray(int[] numbers) {
+        int low = 0;
+        int high = numbers.length - 1;
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (numbers[mid] < numbers[high]) {
+                high = mid;
+            } else if (numbers[mid] > numbers[low]) {
+                low = mid-1;
+            } else {
+                high -= 1;
+            }
+        }
+        return numbers[low];
+    }
+}
